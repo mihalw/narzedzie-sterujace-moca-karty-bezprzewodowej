9df1c9b6f3fc6441e6dffa0570a6368a5d2df6d9
mihalw <michal.machowiak@put.poznan.pl>
2019-06-24 11:37:14 +0200

    test

--- kod.c
+++ kod.c
@@ -7,24 +7,24 @@
 #include <stdlib.h>
 #include <signal.h>
 
-
+#define MAX_QUALITY -20
+#define MIN_QUALITY -85
+#define SLEEP_TIME 2
 
 float signalQuality(float value){
     // percent = 100 x (1 - (PdBm_max - PdBm) / (PdBm_max - PdBm_min)) - wzor z internetu
-    float qualityInPercent = 100 * (1 - (-20 - value) / (-20 - (-85))); 
+    float qualityInPercent = 100 * (1 - (MAX_QUALITY - value) / (MAX_QUALITY - (MIN_QUALITY))); 
     return qualityInPercent;
 }
 
-int sfd;
-
 int main(int argc, char const *argv[])
 {
-    int signal_level;
+    int sfd, signal_level;
     float signal_percent;
     struct iwreq wrq;
     struct iw_statistics stats;
 
-    // stworzenie gniazda aby wywolac polecenie ioctl
+    // stworzenie gniazda aby wywolac pozniej polecenie ioctl
     if((sfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         perror("Could not create socket");
         exit(EXIT_FAILURE);
@@ -38,29 +38,37 @@ int main(int argc, char const *argv[])
         wrq.u.data.pointer = &stats;
         wrq.u.data.length = sizeof(stats);
 
-	ioctl(sfd, SIOCGIWSTATS, &wrq);
+	int wynik3;
+	wynik3 = ioctl(sfd, SIOCGIWSTATS, &wrq);
+	printf("Wynik ioctla: %d\n", wynik3);
+
         signal_level = stats.qual.level - 256; 
         signal_percent = signalQuality(signal_level);
-        printf("Poziom sygnalu [dBm] =  %d (%.2f %%)\n", signal_level, signal_percent);
+        printf("Poziom sygnalu =  %d (%.2f %%) dBm\n", signal_level, signal_percent);
 
         // pobranie wartosci mocy karty bezprzewodowej
-        ioctl(sfd, SIOCGIWTXPOW, &wrq);
-        printf("Obecna wartosc Tx-Power     [dBm] =   %d\n", wrq.u.txpower.value);
+	int wynik2;
+        wynik2 = ioctl(sfd, SIOCGIWTXPOW, &wrq);
+	printf("Wynik ioctla: %d\n", wynik2);
+        printf("Obecna wartosc Tx-Power = %d dBm\n", wrq.u.txpower.value);
 
-
-        if (signal_percent < atoi(argv[3])) {
+        if (signal_percent < atoi(argv[2])) {
             wrq.u.txpower.value++;
-            ioctl(sfd, SIOCSIWTXPOW, &wrq);
-            printf("Zaktualizowana wartosc Tx-Power [dBm] =   %d\n", wrq.u.txpower.value);
+	    int wynik;
+            wynik =ioctl(sfd, SIOCSIWTXPOW, &wrq);
+	    printf("Wynik ioctla: %d\n", wynik);
+            printf("Zaktualizowana wartosc Tx-Power = %d dBm\n", wrq.u.txpower.value);
         }
-        else if (signal_percent > atoi(argv[4])) {
+        else if (signal_percent > atoi(argv[3])) {
             wrq.u.txpower.value--;
-            ioctl(sfd, SIOCSIWTXPOW, &wrq);
-            printf("Zaktualizowana wartosc Tx-Power [dBm] =   %d\n", wrq.u.txpower.value);
+	    int wynik;
+            wynik = ioctl(sfd, SIOCSIWTXPOW, &wrq);
+	    printf("Wynik ioctla: %d\n", wynik);
+            printf("Zaktualizowana wartosc Tx-Power = %d dBm\n", wrq.u.txpower.value);
         }
 
         printf("\n\n");
-        sleep(atoi(argv[2]));
+        sleep(SLEEP_TIME);
     }
 
     close(sfd);
