8fb7cc766d6a11ec95da93e06cffd1ab0d9a661a
mihalw <michal.machowiak@put.poznan.pl>
2019-06-24 10:52:12 +0200

    almost final version

--- kod.c
+++ kod.c
@@ -1,30 +1,68 @@
-#include <stdio.h>
-#include <unistd.h>
-#include "iwlib.h"
-#include <sys/ioctl.h>	
-#include <net/if.h>
-#include <netinet/in.h>
-#include <errno.h>
 #include <string.h>
 #include <stdio.h>
-#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <linux/wireless.h>
+#include <stdlib.h>
+#include <signal.h>
 
-int main(int argc, char *argv[]) {
 
-	int sfd, wynikIoctla, wynikIoctla2;
-	struct iwreq wrq;
 
-	sfd = socket(PF_INET, SOCK_DGRAM, 0);
-	// strncpy (DOKAD, SKAD, ROZMIAR)
-	strncpy(wrq.ifr_name, argv[1], IFNAMSIZ);
-	wynikIoctla = ioctl(sfd, SIOCGIFFLAGS, &wrq.ifr_name);
+float signalQuality(float value){
+    // percent = 100 x (1 - (PdBm_max - PdBm) / (PdBm_max - PdBm_min)) - wzor z internetu
+    float qualityInPercent = 100 * (1 - (-20 - value) / (-20 - (-85))); 
+    return qualityInPercent;
+}
 
-	printf("txpower poczatkowy(powinno byc kolo 20): %d\n", wrq.u.txpower.value);
-	printf("wynik ioctl: %d\n", wynikIoctla);
+int sfd;
 
-	wynikIoctla2 = ioctl(sfd, SIOCSIFFLAGS, wrq.u.txpower.value, 100);
-	printf("txpower po zmianie(powinno byc 100): %d\n", wrq.u.txpower.value);
-	printf("wynik ioctla numer 2: %d\n", wynikIoctla2);
+int main(int argc, char const *argv[])
+{
+    int signal_level;
+    float signal_percent;
+    struct iwreq wrq;
+    struct iw_statistics stats;
 
-	return 0;
+    // stworzenie gniazda aby wywolac polecenie ioctl
+    if((sfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("Could not create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        memset(&wrq, 0, sizeof(wrq)); 
+        memset(&stats, 0, sizeof(stats)); 
+        strncpy(wrq.ifr_name, argv[1], IFNAMSIZ);
+
+        wrq.u.data.pointer = &stats;
+        wrq.u.data.length = sizeof(stats);
+
+	ioctl(sfd, SIOCGIWSTATS, &wrq);
+        signal_level = stats.qual.level - 256; 
+        signal_percent = signalQuality(signal_level);
+        printf("Poziom sygnalu [dBm] =  %d (%.2f %%)\n", signal_level, signal_percent);
+
+        // pobranie wartosci mocy karty bezprzewodowej
+        ioctl(sfd, SIOCGIWTXPOW, &wrq);
+        printf("Obecna wartosc Tx-Power     [dBm] =   %d\n", wrq.u.txpower.value);
+
+
+        if (signal_percent < atoi(argv[3])) {
+            wrq.u.txpower.value++;
+            ioctl(sfd, SIOCSIWTXPOW, &wrq);
+            printf("Zaktualizowana wartosc Tx-Power [dBm] =   %d\n", wrq.u.txpower.value);
+        }
+        else if (signal_percent > atoi(argv[4])) {
+            wrq.u.txpower.value--;
+            ioctl(sfd, SIOCSIWTXPOW, &wrq);
+            printf("Zaktualizowana wartosc Tx-Power [dBm] =   %d\n", wrq.u.txpower.value);
+        }
+
+        printf("\n\n");
+        sleep(atoi(argv[2]));
+    }
+
+    close(sfd);
+    return 0;
 }
